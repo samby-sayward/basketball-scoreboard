55498e7e14c25e1558dadf7cd60358c4213f0d07
samby-sayward <samby.sayward@gmail.com>
2025-09-25 10:31:56 -0700

    Commit initial files

---
+++ index.js
@@ -0,0 +1,42 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+
+let hPoints = 0
+let gPoints = 0
+
+function home1() {
+  hPoints += 1
+  homeScore.textContent = hPoints
+}
+
+function home2() {
+  hPoints += 2
+  homeScore.textContent = hPoints
+}
+
+function home3() {
+  hPoints += 3
+  homeScore.textContent = hPoints
+}
+
+function guest1() {
+  gPoints += 1
+  guestScore.textContent = gPoints
+}
+
+function guest2() {
+  gPoints += 2
+  guestScore.textContent = gPoints
+}
+
+function guest3() {
+  gPoints += 3
+  guestScore.textContent = gPoints
+}
+
+function newGame() {
+  hPoints = 0
+  gPoints = 0
+  homeScore.textContent = hPoints
+  guestScore.textContent = gPoints
+}
